5d16c5eeede54dbb464a75042907f55570cc53be
Felipe Bailez <bailez@usp.br>
2020-04-29 17:38:45 -0300

    setup generic index function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,24 +3,22 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-#detail functions
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
