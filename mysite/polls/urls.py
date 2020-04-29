18be460e037add5ba338a343a0ae5ed16c7434f8
Felipe Bailez <bailez@usp.br>
2020-04-29 17:40:20 -0300

    ajusting urls in polls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name= 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('specifics/<int:question_id>/', views.DetailView.as_view(), name='detail'),
-    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
+    path('specifics/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
